bfcdc9c8b89da2ebb1d594cea65031de11d8c442
Elrabu <busch.elias@web.de>
2025-11-12 19:19:40 +0100

    started working on the base class

--- Assets/BulletScript.cs
+++ Assets/BulletScript.cs
@@ -1,20 +1,15 @@
 using UnityEngine;
 
 public class BulletScript : MonoBehaviour
-{
-
-    public float shootForce = 1000;
+{  
     Rigidbody rigidbody;
-    public float timeToDestroy = 10;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void setUpBullet(float shootForce)
     {
         rigidbody = GetComponent<Rigidbody>();
-        rigidbody.AddForce(transform.forward * shootForce);
+        GetComponent<Rigidbody>().AddForce(transform.forward * shootForce);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RemoveBullet(float timeToDestroy)
     {
         timeToDestroy -= Time.deltaTime;
         if (timeToDestroy <= 0)
