6850d2932dd2ce833c9b61da9aebe02dd7922e79
Elrabu <busch.elias@web.de>
2025-11-24 16:06:58 +0100

    added grabbed object testing #18

---
+++ Assets/GrabListener.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class GrabListener : MonoBehaviour
+{
+    [SerializeField] private UnityEngine.XR.Interaction.Toolkit.Interactors.XRBaseInteractor interactor;
+    private IXRSelectInteractable currentlyHeld;
+
+    public IXRSelectInteractable CurrentlyHeld => currentlyHeld;
+
+    void OnEnable()
+    {
+        interactor.selectEntered.AddListener(OnGrab);
+        interactor.selectExited.AddListener(OnRelease);
+    }
+
+    void OnDisable()
+    {
+        interactor.selectEntered.RemoveListener(OnGrab);
+        interactor.selectExited.RemoveListener(OnRelease);
+    }
+
+    void OnGrab(SelectEnterEventArgs args)
+    {
+        currentlyHeld = args.interactableObject;
+       // Debug.Log("Grabbed: " + args.interactableObject.transform.name);
+    }
+
+    void OnRelease(SelectExitEventArgs args)
+    {
+        currentlyHeld = null;
+       // Debug.Log("Released: " + args.interactableObject.transform.name);
+    }
+}
