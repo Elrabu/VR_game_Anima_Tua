dcbdf61595a5c6128f18d5fce3abb21f5da38bfd
Elrabu <busch.elias@web.de>
2025-12-01 17:27:32 +0100

    improved shooting script #13

--- Assets/Scripts/ShootTestScript.cs
+++ Assets/Scripts/ShootTestScript.cs
@@ -8,6 +8,8 @@ public class ShootTestScript : MonoBehaviour
     public Transform spawnPoint;
     public float secondsBetweenShoot = 0.1f;
     float trackShoot;
+    string gun;
+    [SerializeField] private GrabListener grabListener;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,8 +20,20 @@ public class ShootTestScript : MonoBehaviour
     void Update()
     {
         float value = shoot.action.ReadValue<float>();
+        var item = grabListener.CurrentlyHeld; //get currently held item
+        
+        if (item != null)
+        {
+            gun = item.transform.name;
+            //Debug.Log("Item: " + item.transform.name); 
+        } else
+        {
+            gun = "null";
+        }
         //Debug.Log("Value: " + value);
-        if (value == 1 && trackShoot <= 0)
+        //Debug.Log(gun);
+
+        if (value == 1 && trackShoot <= 0 && gun == gameObject.transform.name)
         {
             Instantiate(bulletPrefab, spawnPoint.position, spawnPoint.rotation);
             trackShoot = secondsBetweenShoot;
