a0425c720c0f89fe72c5f929f5162fe2e643e203
Elrabu <busch.elias@web.de>
2025-12-03 16:19:22 +0100

    improved book grabbing #13

--- Assets/Scripts/GrabListener.cs
+++ Assets/Scripts/GrabListener.cs
@@ -30,26 +30,6 @@ public class GrabListener : MonoBehaviour
     void OnRelease(SelectExitEventArgs args)
     {
         currentlyHeld = null;
-       // Debug.Log("Released: " + args.interactableObject.transform.name);
-    }
-
-    public void DestroyHeldGameObject()
-    {
-        if (currentlyHeld == null)
-        {
-            return;
-        }
-
-        // 1. Tell XRIT to stop selecting it
-        interactor.EndManualInteraction();
-
-        // 2. Cache GameObject reference before clearing
-        GameObject go = currentlyHeld.transform.gameObject;
-
-        // 3. Clear internal reference
-        currentlyHeld = null;
-
-        // 4. Destroy object from scene
-        Destroy(go);
+        Debug.Log("Released: " + args.interactableObject.transform.name);
     }
 }
--- Assets/Scripts/OpenCloseBook.cs
+++ Assets/Scripts/OpenCloseBook.cs
@@ -6,9 +6,13 @@ public class OpenCloseBook : MonoBehaviour
     [SerializeField] private Animator mAnimator;
     [SerializeField] private InputActionProperty interactleft;
     [SerializeField] private InputActionProperty interactright;
+    [SerializeField] private GrabListener grabListenerRight;
+    [SerializeField] private GrabListener grabListenerLeft;
     private bool bookOpen = false;
     private float cooldown = 1f;
     float trackcooldown;
+    string bookRight;
+    string bookLeft;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,12 +25,28 @@ public class OpenCloseBook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float valueLeft = interactleft.action.ReadValue<float>(); //Left
+        float valueRight = interactright.action.ReadValue<float>(); //right
+
+        
         if (mAnimator != null)
         {
-            float value = interactleft.action.ReadValue<float>();
-            float value2 = interactright.action.ReadValue<float>();
+            var itemRight = grabListenerRight.CurrentlyHeld;
+            var itemLeft = grabListenerLeft.CurrentlyHeld;
+            if (itemRight != null)
+            {
+                bookRight = itemRight.transform.name;
+            } else if (itemLeft != null)
+            {
+                bookLeft = itemLeft.transform.name;
+            } else
+            {
+                bookLeft = "null";
+                bookRight = "null";
+            }
+            //Debug.Log("Left: " + bookLeft + "Right: " + bookRight);
         
-            if (value == 1 || value2 == 1)
+            if (valueLeft == 1 && bookLeft == "book" || valueRight == 1 && bookRight == "book")
             {
                 if (bookOpen == false && trackcooldown <= 0) //check for closed book and cooldown 0
                 {
