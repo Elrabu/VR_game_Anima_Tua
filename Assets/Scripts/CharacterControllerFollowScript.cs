93bafa70b469574730a6f1eeb9d0b4919dc89b96
Elrabu <busch.elias@web.de>
2025-12-10 14:02:35 +0100

    fixed belt rotation #13

--- Assets/Scripts/CharacterControllerFollowScript.cs
+++ Assets/Scripts/CharacterControllerFollowScript.cs
@@ -10,5 +10,14 @@ public class CharacterControllerFollowScript : MonoBehaviour
         Vector3 worldOffset = new Vector3(0f, -0.5f, 0f);
         //Debug.Log("Position: " + cam.transform.position);
         transform.position = cam.transform.position + worldOffset;
+
+        Vector3 forward = cam.transform.forward; //world forward position
+
+        forward.y = 0f; //remove pitch and roll influence
+
+        if (forward.sqrMagnitude > 0.01f)
+        {
+            transform.rotation = Quaternion.LookRotation(forward);
+        }
     }
 }
