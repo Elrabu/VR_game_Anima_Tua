ae608ec0f07da864f7811a2dc5e74f0f6247ccbd
Elrabu <busch.elias@web.de>
2026-01-09 13:22:52 +0100

    added cooking #19

---
+++ Assets/CookScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CookScript : MonoBehaviour
+{
+    private GameObject patty;
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.name == "patty" || collision.gameObject.name == "patty(Clone)")
+        {
+            Debug.Log("Interacted with: " + collision.gameObject.name);
+            
+            Transform grilled = collision.transform.Find("patty_grilled");
+            Transform raw = collision.transform.Find("patty_raw");
+
+            if (grilled != null && raw != null)
+            {
+            grilled.gameObject.SetActive(true);
+            raw.gameObject.SetActive(false);
+            }
+        }
+    }
+}
