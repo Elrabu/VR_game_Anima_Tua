1fef13d531687fd1294d17aac9ab7a561bedb56c
Elrabu <busch.elias@web.de>
2026-01-14 15:42:52 +0100

    added tape spawning

--- Assets/NEWBurgercompleteChecker.cs
+++ Assets/NEWBurgercompleteChecker.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class NEWBurgercompleteChecker : MonoBehaviour
 {
+    [SerializeField] private GameObject tape;
+    [SerializeField] private Transform tapespawn;
     private void OnTriggerEnter(Collider other)
     {
         var checker = other.GetComponent<SocketIngredientCheckerScript>();
@@ -12,12 +14,11 @@ public class NEWBurgercompleteChecker : MonoBehaviour
         if (checker.CompletedBurger)
         {
             Debug.Log("Completed burger entered the box!");
-            // Do success logic here
+            Instantiate(tape, tapespawn.position, tapespawn.rotation);
         }
         else
         {
             Debug.Log("Burger not completed yet");
-            // Do failure / reject logic here
         }
     }
 }
