6a3e3cc52258f3d05e085f465dcdbbe74c10cf52
Elrabu <busch.elias@web.de>
2026-01-17 17:33:30 +0100

    updates

--- Assets/Scripts/Burger/CookScript.cs
+++ Assets/Scripts/Burger/CookScript.cs
@@ -1,22 +1,32 @@
 using UnityEngine;
+using System.Collections;
 
 public class CookScript : MonoBehaviour
 {
+    [SerializeField] private GameObject smokePrefab;
     //private GameObject patty;
     void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.name == "patty" || collision.gameObject.name == "patty(Clone)")
         {
-        //    Debug.Log("Interacted with: " + collision.gameObject.name);
-            
-            Transform grilled = collision.transform.Find("patty_grilled");
-            Transform raw = collision.transform.Find("patty_raw");
+            //Debug.Log("Interacted with: " + collision.gameObject.name);
+            Instantiate(smokePrefab, collision.gameObject.transform.position, smokePrefab.transform.rotation);
+            StartCoroutine(Cook(collision.gameObject));
+        }
+    }
 
-            if (grilled != null && raw != null)
-            {
+    IEnumerator Cook(GameObject patty)
+    {
+        // Wait 2 seconds
+        yield return new WaitForSeconds(2f);
+
+        Transform grilled = patty.transform.Find("patty_grilled");
+        Transform raw = patty.transform.Find("patty_raw");
+
+        if (grilled != null && raw != null)
+        {
             grilled.gameObject.SetActive(true);
             raw.gameObject.SetActive(false);
-            }
         }
     }
 }
