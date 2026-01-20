4162eb5299ec6539e8edccb3efa81205085e038e
jg175 <jg175@hdm-stuttgart.de>
2026-01-20 23:26:43 +0100

    feat/fix: Unity crash, implement sound changes after each clip played #63

--- Assets/Scripts/PlaySoundOnEnter.cs
+++ Assets/Scripts/PlaySoundOnEnter.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-public class PlayWatcherSound : StateMachineBehaviour
+
+public class PlaySoundOnEnter : StateMachineBehaviour
 {
     public enum SoundType { None, Floating, Attention, Attack }
     public SoundType soundToPlay = SoundType.None;
@@ -12,17 +13,18 @@ public class PlayWatcherSound : StateMachineBehaviour
 
         switch (soundToPlay)
         {
-            case SoundType.Floating:
-                audio.PlayFloatingLoop();
-                break;
-
-            case SoundType.Attention:
-                audio.PlayAttention();
-                break;
-
-            case SoundType.Attack:
-                audio.PlayAttack();
-                break;
+            case SoundType.Floating:  audio.StartFloating(); break;
+            case SoundType.Attention: audio.PlayAttention(); break;
+            case SoundType.Attack:    audio.PlayAttack();    break;
         }
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        var audio = animator.GetComponent<WatcherAudioController>();
+        if (audio == null) return;
+
+        if (soundToPlay == SoundType.Floating)
+            audio.StopFloating();
     }
 }
