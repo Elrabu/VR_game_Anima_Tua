f5ec18fd20b2d08c9423281f2719279f0c0f3585
Elrabu <busch.elias@web.de>
2026-01-22 11:58:22 +0100

    json testing continued

--- Assets/Scripts/SaveData.cs
+++ Assets/Scripts/SaveData.cs
@@ -3,44 +3,46 @@ using System.Collections.Generic;
 
 public class SaveData : MonoBehaviour
 {
-    public Inventory inventory = new Inventory();
+    public Settings settings = new Settings();
 
     private void Start()
     {
         //SaveToJson();
-        //LoadFromJson();
+        LoadFromJson();
     }
 
     public void SaveToJson()
     {
-        string inventoryData = JsonUtility.ToJson(inventory);
-        string filePath = Application.persistentDataPath + "/InventoryData.json";
+        string settingsData = JsonUtility.ToJson(settings);
+        string filePath = Application.persistentDataPath + "/SettingsData.json";
         Debug.Log(filePath);
-        System.IO.File.WriteAllText(filePath, inventoryData);
-        Debug.Log("data read!");
+        System.IO.File.WriteAllText(filePath, settingsData);
+        Debug.Log("data written!");
     }
 
     public void LoadFromJson()
     {
-        string filePath = Application.persistentDataPath + "/InventoryData.json";
-        string inventoryData = System.IO.File.ReadAllText(filePath);
+        string filePath = Application.persistentDataPath + "/SettingsData.json";
+        string settingsData = System.IO.File.ReadAllText(filePath);
 
-        inventory = JsonUtility.FromJson<Inventory>(inventoryData);
+        settings = JsonUtility.FromJson<Settings>(settingsData);
         Debug.Log("Data loaded");
+        Debug.Log(settings.snapTurnEnabled +""+ settings.continuousTurnEnabled +""+ settings.tunnelingVignetteEnabled +""+ settings.levels[0]);
     }
 }
 
 [System.Serializable]
-public class Inventory
+public class Settings
 {
-    public int goldCoins = 100;
-    public bool isFull = true;
-    public List<items> items = new List<items>();
+    public bool snapTurnEnabled = false;
+    public bool continuousTurnEnabled = false;
+    public bool tunnelingVignetteEnabled = false;
+    public List<Levels> levels = new List<Levels>();
 }
 
 [System.Serializable]
-public class items
+public class Levels
 {
     public string name;
-    public string desc;
+    public bool completed;
 } 
