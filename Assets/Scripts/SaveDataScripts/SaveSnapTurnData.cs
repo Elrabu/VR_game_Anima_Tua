184418ecfd94b4873bee6bb92af47cbdca7d4db7
Elrabu <busch.elias@web.de>
2026-01-24 23:01:45 +0100

    finished checkboxes

--- Assets/Scripts/SaveDataScripts/SaveSnapTurnData.cs
+++ Assets/Scripts/SaveDataScripts/SaveSnapTurnData.cs
@@ -5,6 +5,8 @@ public class SaveSnapTurnData : MonoBehaviour
 {
     private bool snapTurn = false;
     [SerializeField] private Toggle toggle;
+    [SerializeField] private Toggle Continuoustoggle;
+    [SerializeField] private GameObject snapturnProvider;
     public void Start()
     {
         SaveData.Instance.LoadFromJson();
@@ -18,12 +20,15 @@ public class SaveSnapTurnData : MonoBehaviour
         if (snapTurn)
         {
             snapTurn = false;
+            snapturnProvider.SetActive(false);
             SaveData.Instance.settings.snapTurnEnabled = false;
             SaveData.Instance.SaveToJson();
             Debug.Log("disabledSnapTurn");
         } else
         {
             snapTurn = true;
+            snapturnProvider.SetActive(true);
+            Continuoustoggle.isOn = false;
             SaveData.Instance.settings.snapTurnEnabled = true;
             SaveData.Instance.SaveToJson();
             Debug.Log("enabledSnapTurn");
