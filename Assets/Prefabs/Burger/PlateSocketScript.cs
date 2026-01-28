9b8717e77e6af6d5abc8f64e08391e5a42fac942
Maximilian Scherbaum <ms622@hdm-stuttgart.de>
2026-01-28 21:27:47 +0100

    Implemented diner quest audio guidence

--- Assets/Prefabs/Burger/PlateSocketScript.cs
+++ Assets/Prefabs/Burger/PlateSocketScript.cs
@@ -8,6 +8,13 @@ public class PlateSocketScript : MonoBehaviour
     [SerializeField] private UnityEngine.XR.Interaction.Toolkit.Interactors.XRSocketInteractor socket;
     private bool spawned = false;
     private bool plate = false;
+
+    private AudioSource serveOrderAudio;
+    private AudioSource jukeboxDistAudio;
+    private AudioSource jukeboxNormAudio;
+
+    private Light devLight;
+
     public bool filledplate => plate;
 
     private void OnEnable()
@@ -52,17 +59,55 @@ public class PlateSocketScript : MonoBehaviour
             if (spawned == false)
             {
                 Debug.Log("Completed burger!");
-              //  Instantiate(tape, tapespawn.position, tapespawn.rotation);
-                                                                                                                                                // 
-                                                                                                                                                // Sound change here
-                                                                                                                                                //
-                spawned = true;
-                plate = true;
-            }  
+                //  Instantiate(tape, tapespawn.position, tapespawn.rotation);
+                // 
+                // Sound change here
+                //
+                handleQuest();
+            }
+
+            spawned = true;
+            plate = true;
+            
         }
         else
         {
             Debug.Log("Burger not completed yet");
+        }
+    }
+
+    private void handleQuest()
+    {
+        GameObject serveOrderObject = GameObject.Find("ServeOrder");
+        GameObject jukeboxDistObject = GameObject.Find("JukeboxDistortedAudio");
+        GameObject jukeboxNormObject = GameObject.Find("JukeboxNormalAudio");
+
+        GameObject delivLightObj = GameObject.Find("DeliveryLight");
+
+        if (serveOrderObject != null && jukeboxDistObject != null)
+        {
+            serveOrderAudio = serveOrderObject.GetComponent<AudioSource>();
+            jukeboxDistAudio = jukeboxDistObject.GetComponent<AudioSource>();
+            jukeboxNormAudio = jukeboxNormObject.GetComponent<AudioSource>();
+
+            devLight = delivLightObj.GetComponent<Light>();
+
+            if (serveOrderAudio == null)
+            {
+                Debug.LogWarning("ServeOrder GameObject found, but it has no AudioSource component!");
+            }
+            else
+            {
+                serveOrderAudio.Play();
+                jukeboxDistAudio.volume = 1.0f;
+                jukeboxNormAudio.volume = 0.0f;
+
+                devLight.intensity = 40000.0f;
+            }
+        }
+        else
+        {
+            Debug.LogWarning("ServeOrder GameObject not found in the scene!");
         }
     }
 }
